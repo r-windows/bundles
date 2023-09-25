7e67d923178340e3f600dca72f1909281b7694c7
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-25 13:34:25 +0200

    Only allow overwriting if requested

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 source lib/functions.sh
-export package="$1"
 if [ -f "${package}.sh" ]; then
   source ./${package}.sh
 fi
--- lib/functions.sh
+++ lib/functions.sh
@@ -22,6 +22,20 @@ download_libs(){
   echo "Bundling: $pkg $version"
   #echo "Skiplist: $skiplist"
 
+  # Check if version already exists
+  tagurl="https://github.com/r-windows/bundles/releases/tag/$package-$version"
+  status=$(curl -sSLIw '%{http_code}' -o /dev/null $tagurl);
+  if [ "$status" = "404" ]; then
+    echo "Tag does not exist yet. Good."
+  else
+    echo "Tag already exists: $tagurl"
+    if [ "$overwrite" = "true" ]; then
+      echo "Overwriting as requested"
+    else
+      exit 1
+    fi
+  fi
+
   # Find dependencies
   if [ "$deps" ]; then
     pkgdeps=$(arch_prefix $deps)
